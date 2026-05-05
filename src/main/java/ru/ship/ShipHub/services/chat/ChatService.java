4b17ecdae6e0437bb54dcaac08374216a5619251
Дмитрий <bosy.dmitry1@mail.ru>
2026-05-06 01:36:30 +0300

    чат работает, но с некоторыми недочётами

--- src/main/java/ru/ship/ShipHub/services/chat/ChatService.java
+++ src/main/java/ru/ship/ShipHub/services/chat/ChatService.java
@@ -2,21 +2,24 @@ package ru.ship.ShipHub.services.chat;
 
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.ship.ShipHub.ChatServiceGrpc;
 import ru.ship.ShipHub.ClaimMessageService;
 import ru.ship.ShipHub.repositories.ClaimRepository;
 import ru.ship.ShipHub.util.exceptions.BadRequestException;
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.HashMap;
+import java.util.Map;
 
 @GrpcService
 @Service
 public class ChatService extends ChatServiceGrpc.ChatServiceImplBase {
 
+    private static final Logger log = LoggerFactory.getLogger(ChatService.class);
     private final ClaimRepository claimRepository;
-
-    private ConcurrentHashMap<Long, StreamObserver<ClaimMessageService.MessageRequest>> subscribers = new ConcurrentHashMap<>();
+    private final HashMap<Long, HashMap<Long, StreamObserver<ClaimMessageService.Message>>> subscribers = new HashMap<>();
 
     public ChatService(ClaimRepository claimRepository) {
         this.claimRepository = claimRepository;
@@ -28,14 +31,46 @@ public class ChatService extends ChatServiceGrpc.ChatServiceImplBase {
             @Override
             public void onNext(ClaimMessageService.MessageRequest request) {
                 if (request.getIsSubscribeRequest()){
-                    if (subscribers.contains(request.getUserId())) throw new BadRequestException("Пользователь уже подписан на этот чат");
-                    System.out.println(request.getContent());
+                    var claimOptional = claimRepository.findById(request.getClaimId());
+                    if (claimOptional.isEmpty()){
+                        onError(new Throwable("Заявка с таким id не найдена"));
+                        return;
+                    }
+                    var claim = claimOptional.get();
+                    var subscribe = subscribers.get(claim.getId());
+                    if (subscribe != null && subscribe.containsKey(request.getUserId())) {
+                        onError(new Throwable("Пользователь уже подписан на этот чат"));
+                    }
+                    else {
+                        var newSubscribe = new HashMap<Long, StreamObserver<ClaimMessageService.Message>>();
+                        newSubscribe.put(request.getUserId(), responseObserver);
+                        var currentSubscribe = subscribers.get(claim.getId());
+                        if (currentSubscribe != null){
+                            currentSubscribe.put(request.getUserId(), responseObserver);
+                            subscribers.put(claim.getId(), currentSubscribe);
+                        }else{
+                            subscribers.put(claim.getId(), newSubscribe);
+                        }
+                        var response = ClaimMessageService.Message.newBuilder()
+                                .setContent("Success")
+                                .build();
+                        responseObserver.onNext(response);
+                    }
+                }else {
+                    if (request.getContent().isEmpty()) return;
+                    var message = ClaimMessageService.Message.newBuilder()
+                            .setContent(request.getContent())
+                            .build();
+                    broadcastMessage(message, request.getClaimId(), request.getUserId());
                 }
             }
 
             @Override
             public void onError(Throwable throwable) {
-
+                var message = ClaimMessageService.Message.newBuilder()
+                        .setContent("error: " + throwable.getLocalizedMessage())
+                        .build();
+                responseObserver.onNext(message);
             }
 
             @Override
@@ -45,7 +80,15 @@ public class ChatService extends ChatServiceGrpc.ChatServiceImplBase {
         };
     }
 
-    private void validateMessage(ClaimMessageService.MessageRequest message){
-
+    private void broadcastMessage(ClaimMessageService.Message message, Long claimId, Long sender){
+        var chatSubscribers = subscribers.get(claimId);
+        if (chatSubscribers == null) return;
+        for (Map.Entry<Long, StreamObserver<ClaimMessageService.Message>> entry : chatSubscribers.entrySet()) {
+            if (!entry.getKey().equals(sender)) {
+                entry.getValue().onNext(message);
+            }
+        }
     }
+
+    private void badRequest(String error){ throw new BadRequestException(error); }
 }
